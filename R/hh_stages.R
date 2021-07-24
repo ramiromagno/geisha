a2e1c45ad2e5895b8eeee5ffcf19f0204b0103ea
Ramiro Magno <ramiro.magno@gmail.com>
2021-07-24 10:24:32 +0100

    hh_stages fn comprises now stages X thru 46

--- R/hh_stages.R
+++ R/hh_stages.R
@@ -9,10 +9,10 @@
 #' @return A character vector of Hamilton-Hamburguer (HH) stages.
 #' @export
 hh_stages <- function(hh_prefix = FALSE) {
-
+# https://embryology.med.unsw.edu.au/embryology/index.php?title=Hamburger_Hamilton_Stages
   if(hh_prefix) {
-    return(c(as.character(utils::as.roman(10:14)), paste0('HH', as.character(c(1:44)))))
+    return(c(as.character(utils::as.roman(10:14)), paste0('HH', as.character(c(1:46)))))
   } else {
-    return(c(as.character(utils::as.roman(10:14)), as.character(c(1:44))))
+    return(c(as.character(utils::as.roman(10:14)), as.character(c(1:46))))
   }
 }
